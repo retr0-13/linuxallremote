2a2ceea8e7539c28ae9bbf4556881876b10559c0
FabioDefilippo <71832873+FabioDefilippo@users.noreply.github.com>
2020-11-27 14:27:34 +0100

    Update linuxallremote.sh

--- linuxallremote.sh
+++ linuxallremote.sh
@@ -388,7 +388,7 @@ while true; do
 	echo -ne " 214. j3ssie/Osmedeus\t\t\t\t242. smicallef/spiderfoot\t\t\t\t308. yogeshojha/rengine\n"
 	echo -ne " 390. lanmaster53/recon-ng\t\t\t391. methos2016/recon-ng\t\t\t\t501. LukaSikic/subzy\n"
 	echo -ne " 556. LordNeoStark/tugarecon\t\t\t594. r3vn/badKarma\t\t\t\t\t599. utkusen/urlhunter\n"
-	echo -ne " 601. UnaPibaGeek/ctfr\n"
+	echo -ne " 601. UnaPibaGeek/ctfr\t\t\t\t607. thewhiteh4t/seeker\n"
 	echo "$SEP"
 	echo "REVERSING"
 	echo -ne " 361. yeggor/UEFI_RETool\n"
@@ -3220,6 +3220,9 @@ while true; do
 	;;
 	"606")
 		Clona "Chudry/Xerror"
+	;;
+	"607")
+		Clona "thewhiteh4t/seeker"
 	;;
 	*)
 		echo "error, invalid choice"
