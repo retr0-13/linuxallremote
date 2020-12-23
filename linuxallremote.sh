6a6f58fc85bb2b11d3bcc0de0f68e6af68b42310
FabioDefilippo <71832873+FabioDefilippo@users.noreply.github.com>
2020-12-23 23:09:02 +0100

    Update linuxallremote.sh

--- linuxallremote.sh
+++ linuxallremote.sh
@@ -262,7 +262,7 @@ while true; do
 	echo -ne " 486. sowdust/tafferugli\t\t\t537. adnane-X-tebbaa/Katana\t\t\t\t555. m8r0wn/subscraper\n"
 	echo -ne " 560. Datalux/Osintgram\t\t\t\t585. thewhiteh4t/FinalRecon\t\t\t\t588. AzizKpln/Moriarty-Project\n"
 	echo -ne " 589. mxrch/GHunt\t\t\t\t613. bdblackhat/admin-panel-finder\t\t\t\t625. TermuxHacking000/phonia\n"
-	echo -ne " 631. Anon-Exploiter/SiteBroker\n"
+	echo -ne " 631. Anon-Exploiter/SiteBroker\t\t\t646. nandydark/grim\n"
 	echo "$SEP"
 	echo "GIT - REPOS"
 	echo -ne " 487. arthaud/git-dumper\t\t\t553. Ebryx/GitDump\n"
@@ -3482,6 +3482,9 @@ while true; do
 	;;
 	"645")
 		Clona "foozzi/iCloudBrutter"
+	;;
+	"646")
+		Clona "nandydark/grim"
 	;;
 	*)
 		echo "error, invalid choice"
