ea54f4c4f5a004bfa0a3f7bd237426789663e88e
FabioDefilippo <71832873+FabioDefilippo@users.noreply.github.com>
2021-01-08 16:33:46 +0100

    Update linuxallremote.sh

--- linuxallremote.sh
+++ linuxallremote.sh
@@ -382,7 +382,7 @@ while true; do
 	echo -ne " 554. FluxionNetwork/fluxion\t\t\t557. knassar702/scant3r\t\t\t\t\t567. Leviathan36/kaboom\n"
 	echo -ne " 568. archerysec/archerysec\t\t\t579. AnonymousAt3/cybermap\t\t\t\t604. qsecure-labs/overlord\n"
 	echo -ne " 606. Chudry/Xerror\t\t\t\t616. rajkumardusad/Tool-X\t\t\t\t626. GoVanguard/legion\n"
-	echo -ne " 640. KALILINUXTRICKSYT/easysploit\t\t650. edoardottt/scilla\n"
+	echo -ne " 640. KALILINUXTRICKSYT/easysploit\t\t650. edoardottt/scilla\t\t\t\t742. rajkumardusad/onex\n"
 	echo "$SEP"
 	echo "MITM"
 	echo -ne " 249. kgretzky/evilginx2\t\t\t331. mkdirlove/SSLSTRIP-NG/sslstrip-ng\t\t\t541. wifiphisher/wifiphisher\n"
@@ -3881,6 +3881,9 @@ while true; do
 	;;
 	"741")
 		Clona "PaytmLabs/nerve"
+	;;
+	"742")
+		Clona "rajkumardusad/onex"
 	;;
 	*)
 		echo "error, invalid choice"
