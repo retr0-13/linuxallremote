6c9365afcb6862413c8992f50c3a732788c9136b
FabioDefilippo <71832873+FabioDefilippo@users.noreply.github.com>
2021-03-09 14:17:06 +0100

    Update linuxallremote.sh

--- linuxallremote.sh
+++ linuxallremote.sh
@@ -462,7 +462,7 @@ while true; do
 	echo -ne " 1002. lightos/credmap\t\t\t\t1005. ilektrojohn/creepy\t\t\t\t1042. DanMcInerney/device-pharmer\n"
 	echo -ne " 1076. utiso/dorkbot\t\t\t\t1077. blueudp/DorkMe\t\t\t\t\t1078. NullArray/DorkNet\n"
 	echo -ne " 1118. martinvigo/email2phonenumber\t\t2302. josh0xA/darkdump\t\t\t\t2314. FortyNorthSecurity/Just-Metadata\n"
-	echo -ne " 2316. davidtavarez/pwndb\t\t\t2326. j3ers3/Searpy\n"
+	echo -ne " 2316. davidtavarez/pwndb\t\t\t2326. j3ers3/Searpy\t\t\t\t\t1628. behindthefirewalls/Parsero\n"
 	echo "$SEP"
 	echo "GIT - REPOS"
 	echo -ne " 487. arthaud/git-dumper\t\t\t553. Ebryx/GitDump\t\t\t\t\t2315. metac0rtex/GitHarvester\n"
