66c41a7788f6b4160af30dea03498a03f056265b
FabioDefilippo <71832873+FabioDefilippo@users.noreply.github.com>
2021-04-03 16:48:55 +0200

    Update linuxallremote.sh

--- linuxallremote.sh
+++ linuxallremote.sh
@@ -103,7 +103,7 @@ function Clona
 {
 	DKF="Dockerfile"
 	echo "Choose what version you want to download ""$1"
-	if [[ $(wget -q -S --spider "$ENTRAW""$1""/master/""$DKF" 2>&1) == *"200"* || $(wget -q -S --spider "$ENTRAW""$1""/main/""$DKF" 2>&1) == *"200"* ]];
+	if [[ $(wget -q -S --spider "$ENTRAW""$1""/master/""$DKF" 2>&1) == *"200 OK"* || $(wget -q -S --spider "$ENTRAW""$1""/main/""$DKF" 2>&1) == *"200 OK"* ]];
 	then
 		echo "1. Dockerfile"
 	fi
@@ -117,11 +117,11 @@ function Clona
 	read -p "(default 3): " CVR
 	case "$CVR" in
 	"1")
-		if [[ $(wget -q -S --spider "$ENTRAW""$1""/master/""$DKF" 2>&1) == *"200"* ]];
+		if [[ $(wget -q -S --spider "$ENTRAW""$1""/master/""$DKF" 2>&1) == *"200 OK"* ]];
 		then
 			Scarica "$ENTRAW""$1""/master/""$DKF" "$DKF"
 		else
-			if [[ $(wget -q -S --spider "$ENTRAW""$1""/main/""$DKF" 2>&1) == *"200"* ]];
+			if [[ $(wget -q -S --spider "$ENTRAW""$1""/main/""$DKF" 2>&1) == *"200 OK"* ]];
 			then
 				Scarica "$ENTRAW""$1""/main/""$DKF" "$DKF"
 			else
