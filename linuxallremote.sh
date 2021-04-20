ffdcb0475b3a8d4e6085db51ea9b5d0a19c676be
FabioDefilippo <71832873+FabioDefilippo@users.noreply.github.com>
2021-04-20 12:14:57 +0200

    Update linuxallremote.sh

--- linuxallremote.sh
+++ linuxallremote.sh
@@ -908,7 +908,7 @@ while true; do
 	echo -ne " 776. cSploit/android\t\t\t\t777. routerkeygen/routerkeygenAndroid\t\t\t782. intercepter-ng\n"
 	echo -ne " 2234. OnionApps/Chat.onion\t\t\t2254. LinkClink/Rainbow-Wifi-Hack-Utility-Android\t2255. trevatk/Wifi-Cracker\n"
 	echo -ne " 2256. trevatk/Wifi-Cracker\t\t\t2257. faizann24/wifi-bruteforcer-fsecurify\t\t2258. faizann24/wifi-bruteforcer-fsecurify\n"
-	echo -ne " 2412. urbanadventurer/Android-PIN-Bruteforce\n"
+	echo -ne " 2412. urbanadventurer/Android-PIN-Bruteforce\t\t\t2450. modded-ubuntu/modded-ubuntu\n"
 	echo "$SEP"
 	echo "TFTP"
 	echo -ne " 719. nullsecuritynet/tftp-fuzz\n"
@@ -9825,6 +9825,9 @@ while true; do
 	;;
 	"2449")
 		Scarica "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/spraygen/spraygen-1.3.4.zip"
+	;;
+	"2450")
+		Clona "modded-ubuntu/modded-ubuntu"
 	;;
 	*)
 		echo "error, invalid choice"
