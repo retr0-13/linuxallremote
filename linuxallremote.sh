2af7878b0dc6adaf82f66fe880695dfd505aa8de
FabioDefilippo <71832873+FabioDefilippo@users.noreply.github.com>
2021-08-09 02:55:56 +0200

    Update linuxallremote.sh

--- linuxallremote.sh
+++ linuxallremote.sh
@@ -599,7 +599,7 @@ while true; do
 	Stampa " 2345. SharadKumar97/OSINT-SPY" "2439. matamorphosis/Scrummage" "1182. galkan/flashlight"
 	Stampa " 1187. byt3smith/Forager" "1240. Nhoya/gOSINT" "1271. Te-k/harpoon"
 	Stampa " 2500. jakejarvis/awesome-shodan-queries" "2501. interference-security/zoomeye-data" "2526. A3h1nt/Grawler"
-	Stampa " 2534. sc1341/TikTok-OSINT"
+	Stampa " 2534. sc1341/TikTok-OSINT" "2538. nccgroup/Solitude"
 	echo "$SEP"
 	echo "GIT - REPOS"
 	Stampa " 487. arthaud/git-dumper" "553. Ebryx/GitDump" "2315. metac0rtex/GitHarvester"
